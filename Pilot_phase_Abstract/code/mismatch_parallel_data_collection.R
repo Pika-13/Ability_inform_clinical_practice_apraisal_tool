d286f2ab2969f6f503e7e39a68c8722567ac3f74
Pika-13 <joanarodriguesribeiro1998@gmail.com>
2026-06-12 14:25:12 +0200

    share PhDMID

--- Pilot_phase_Abstract/code/mismatch_parallel_data_collection.R
+++ Pilot_phase_Abstract/code/mismatch_parallel_data_collection.R
@@ -50,32 +50,33 @@ compare_dataframes <- function(df1, df2) {
 }
 # Example usage:
 dfFA <- janitor::clean_names(
-  read_xlsx("database/Revised_Data_original_Fariba_2025_10_12_compared.xlsx",col_names = TRUE)[-1, ]) 
+  read_xlsx("Pilot_phase_Abstract/database/Revised_Data_original_Fariba_2025_10_30.xlsx",col_names = TRUE)[-1, ]) 
 dfJRR <- janitor::clean_names(
-  read_xlsx("database/Revised_Data_original_Joana_2025_10_12_compared.xlsx", col_names = TRUE)[-1, ]) 
+  read_xlsx("Pilot_phase_Abstract/database/Revised_Data_original_Joana_2025_10_30.xlsx", col_names = TRUE)[-1, ]) 
 
 #columns of interest
 interest_col <- c("article_id", "title", 
                  # "cd_cci_reported_in_results",
-                  "primary_outcome_significant",
-                  "statistically_significant_difference_for_cci"  ,
-                  "between_group_difference_of_cci" , 
-                  "mean_or_median_used", 
-                  "cci_used_for_ss_calculation_in_paper",                              
+                 # "primary_outcome_significant",
+                 # "statistically_significant_difference_for_cci"  ,
+                 # "between_group_difference_of_cci" , 
+                 # "mean_or_median_used", 
+                 # "cci_used_for_ss_calculation_in_paper",                              
                   "specification_of_ss_calculation_in_paper",                          
                   "study_type_mentioned",
                   "type_of_study" ,                                                    
-                  "definition_of_non_inf_margin",
-                  "study_must_be_excluded_e_g_protocol_whose_study_has_been_conducted",
-                  "doi_protocol","publication_year_protocol" ,
-                  "cci_in_ss_calculation_of_protocol" ,
+                 # "definition_of_non_inf_margin",
+                #  "study_must_be_excluded_e_g_protocol_whose_study_has_been_conducted",
+                #  "doi_protocol","publication_year_protocol" ,
+                 # "cci_in_ss_calculation_of_protocol" ,
                   "specification_of_ss_calculation",                          
                   "study_type_mentioned_protocol",
-                  "type_of_study_protocol" ,                                                    
-                  "definition_of_non_inf_margin_protocol"
+                  "type_of_study_protocol_1st_ep" ,                                                    
+                 # "definition_of_non_inf_margin_protocol",
+                "significance_considered"
 )
 
-control_12_october_FA_jrr <- compare_dataframes(
+control_30_october_FA_jrr <- compare_dataframes(
   dfFA[interest_col], 
  # compared_RCT[interest_col], 
   dfJRR[interest_col]) %>% 
@@ -83,5 +84,62 @@ control_12_october_FA_jrr <- compare_dataframes(
    dfFA %>% select(article_id), 
     by = c("article_id" = "article_id")
   )
-write_xlsx(control_12_october_FA_jrr, "database/csv_files_R_coding/control_12_october_FA_jrr.xlsx")
+write_xlsx(control_30_october_FA_jrr, "Pilot_phase_Abstract/database/csv_files_R_coding/control_30_october_FA_jrr.xlsx")
+control_30_october_FA_jrr %>% filter(!is.na(df2_value) & df2_value!="na") %>% filter(df1_value!=df2_value) %>% nrow()
+
+#kappa results interpretation
+table_agreement <- dfJRR %>% 
+  transmute(article_id,
+            significance_considered_JRR=significance_considered,
+            study_type_mentioned_JRR = study_type_mentioned,
+            study_type_mentioned_protocol_JRR=study_type_mentioned_protocol) %>%  
+  left_join(
+  dfFA %>% select(protocol, study_must_be_excluded_e_g_protocol_whose_study_has_been_conducted, 
+                  article_id, significance_considered,
+                  study_type_mentioned,study_type_mentioned_protocol), 
+  by = c("article_id" = "article_id")
+) %>% 
+  filter(protocol==0 & 
+           study_must_be_excluded_e_g_protocol_whose_study_has_been_conducted==0) %>% 
+  select(article_id,significance_considered, significance_considered_JRR,
+         study_type_mentioned,study_type_mentioned_protocol,
+         study_type_mentioned_JRR,study_type_mentioned_protocol_JRR,
+  ) %>% 
+  transmute(article_id,
+            significance_considered=case_when(
+    significance_considered==1 ~1,
+    str_detect(significance_considered, "0") ~0,
+    significance_considered=="na" ~0,
+    .default = 0),
+    significance_considered_JRR=case_when(
+      significance_considered_JRR==1 ~1,
+      str_detect(significance_considered_JRR, "0") ~0,
+      significance_considered_JRR=="na"~0,
+      .default = 0),
+    study_type_mentioned=case_when(
+      study_type_mentioned==1 ~1,
+      study_type_mentioned==0 ~0,
+      .default = NA),
+    study_type_mentioned_JRR=case_when(
+      study_type_mentioned_JRR==1 ~1,
+      study_type_mentioned_JRR==0 ~0,
+      .default = NA),
+    study_type_mentioned_protocol=case_when(
+      study_type_mentioned_protocol==1 ~1,
+      study_type_mentioned_protocol==0 ~0,
+      .default = NA),
+    study_type_mentioned_protocol_JRR=case_when(
+      study_type_mentioned_protocol_JRR==1 ~1,
+      study_type_mentioned_protocol_JRR==0 ~0,
+      .default = NA),
+  ) 
+
+#kappa for significance
+contingtable <- table(
+  table_agreement$significance_considered_JRR,
+  table_agreement$significance_considered
+  )
+library(psych)
+psych::cohen.kappa(contingtable)
+
 
